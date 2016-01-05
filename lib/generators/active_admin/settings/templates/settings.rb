bc6b01b8ae6928857a623e6435daf4939cb67d37
Alexander Merkulov <sasha@merqlove.ru>
2016-01-05 19:08:06 +0300

    Handy page title.

--- lib/generators/active_admin/settings/templates/settings.rb
+++ lib/generators/active_admin/settings/templates/settings.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register_page '<%= class_name %>' do
-  menu label: '<%= class_name %>', priority: 99, id: 'settings'
-  active_admin_settings(title: '<%= class_name %>')
+  title = '<%= class_name %>'
+  menu label: title
+  active_admin_settings(title: title)
 end
