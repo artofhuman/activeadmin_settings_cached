a1a01c6710f00827cf6c27f01dd769d8ba027df2
Alexander Merkulov <sasha@merqlove.ru>
2017-07-31 20:58:15 +0300

    Fix generator for AA.

--- lib/generators/active_admin/settings_generator.rb
+++ lib/generators/active_admin/settings_generator.rb
@@ -1,4 +1,4 @@
-module ActiveadminSettingsCached
+module ActiveAdmin
   module Generators
     class SettingsGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../templates', __FILE__)
