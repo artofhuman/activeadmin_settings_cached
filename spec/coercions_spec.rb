c9f7558ac0f968ea540ae13afacd12a21153e5c9
Semyon Pupkov <mail@semyonpupkov.com>
2018-05-22 11:40:31 +0500

    Rewrite coercions spec

--- spec/coercions_spec.rb
+++ spec/coercions_spec.rb
@@ -21,60 +21,66 @@ RSpec.describe ActiveadminSettingsCached::Coercions do
       'base.five_setting'     => :aaa,
       'second.first_setting'  => false,
       'second.second_setting' => 'BBB',
-      'some'                  => Hash.new
+      'some'                  => {}
     ].with_indifferent_access
   end
-
-  let(:right_params) do
-    ActionController::Parameters.new(
-      Hash[
-        'base.first_setting'    => 'BBB',
-        'base.second_setting'   => 'false',
-        'base.third_setting'    => '155',
-        'base.four_setting'     => '55.5',
-        'base.five_setting'     => 'bbb',
-        'second.first_setting'  => 'true',
-        'second.second_setting' => 'AAA'
-      ]
-    )
-  end
-
-  let(:wrong_params) do
-    ActionController::Parameters.new(
-      Hash['base.second_setting'  => 'hjgj',
-           'base.third_setting'   => 'fhfh',
-           'base.four_setting'    => 'gjfhg',
-           'second.first_setting' => 'ggf',
-           'some'                 => Hash.new
-    ])
-  end
-
-  let(:no_params) { ActionController::Parameters.new(Hash[]) }
 
   subject(:coercions) do
     ActiveadminSettingsCached::Coercions.new(defaults, display)
   end
 
-  it 'when good params' do
-    expect { |b| coercions.cast_params(right_params, &b) }
-      .to yield_successive_args(['base.first_setting', 'BBB'],
-                                ['base.second_setting', false],
-                                ['base.third_setting', 155],
-                                ['base.four_setting', 55.5],
-                                ['base.five_setting', :bbb],
-                                ['second.first_setting', true],
-                                ['second.second_setting', 'AAA'])
+  context 'when params is valid' do
+    let(:params) do
+      ActionController::Parameters.new(
+        Hash[
+          'base.first_setting'    => 'BBB',
+          'base.second_setting'   => 'false',
+          'base.third_setting'    => '155',
+          'base.four_setting'     => '55.5',
+          'base.five_setting'     => 'bbb',
+          'second.first_setting'  => 'true',
+          'second.second_setting' => 'AAA'
+        ]
+      )
+    end
+
+    it 'casts params' do
+      expect { |b| coercions.cast_params(params, &b) }
+        .to yield_successive_args(['base.first_setting', 'BBB'],
+                                  ['base.second_setting', false],
+                                  ['base.third_setting', 155],
+                                  ['base.four_setting', 55.5],
+                                  ['base.five_setting', :bbb],
+                                  ['second.first_setting', true],
+                                  ['second.second_setting', 'AAA'])
+    end
   end
 
-  it 'when wrong params' do
-    expect { |b| coercions.cast_params(wrong_params, &b) }
-      .to yield_successive_args(['base.second_setting', false],
-                                ['base.third_setting', 0],
-                                ['base.four_setting', 0.0],
-                                ['second.first_setting', false])
+  context 'when params is wrong' do
+    let(:params) do
+      ActionController::Parameters.new(
+        Hash[
+          'base.second_setting'  => 'hjgj',
+          'base.third_setting'   => 'fhfh',
+          'base.four_setting'    => 'gjfhg',
+          'second.first_setting' => 'ggf',
+          'some'                 => {}
+        ]
+      )
+    end
+
+    it 'returns default values' do
+      expect { |b| coercions.cast_params(params, &b) }
+        .to yield_successive_args(['base.second_setting', false],
+                                  ['base.third_setting', 0],
+                                  ['base.four_setting', 0.0],
+                                  ['second.first_setting', false])
+    end
   end
 
-  it 'when no params' do
-    expect { |b| coercions.cast_params(no_params, &b) }.not_to yield_control
+  context 'when params is empty' do
+    let(:params) { ActionController::Parameters.new(Hash[]) }
+
+    it { expect { |b| coercions.cast_params(params, &b) }.not_to yield_control }
   end
 end
