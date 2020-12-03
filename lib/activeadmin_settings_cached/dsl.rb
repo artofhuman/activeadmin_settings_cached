cc6ea3bbc3db65a61f0c4824058140b1284e470c
Semyon Pupkov <mail@semyonpupkov.com>
2020-12-03 10:38:18 +0500

    Use rails-settings-cached >= 2.0

--- lib/activeadmin_settings_cached/dsl.rb
+++ lib/activeadmin_settings_cached/dsl.rb
@@ -8,8 +8,6 @@ module ActiveadminSettingsCached
     #
     # @param [Hash] options
     # @option options [String] :model_name, settings model name override (default: uses name from global config.)
-    # @option options [String] :starting_with, each key must starting with, (default: nil)
-    # @option options [String] :key, root key can be replacement for starting_with, (default: nil)
     # @option options [String] :template custom, template rendering (default: 'admin/settings/index')
     # @option options [String] :template_object, object to use in templates (default: ActiveadminSettingsCached::Model instance)
     # @option options [String] :display, display settings override (default: nil)
@@ -19,8 +17,8 @@ module ActiveadminSettingsCached
       options.assert_valid_keys(*ActiveadminSettingsCached::Options::VALID_OPTIONS)
 
       options = ActiveadminSettingsCached::Options.options_for(options)
-      coercion =
-        ActiveadminSettingsCached::Coercions.new(options[:template_object].defaults, options[:template_object].display)
+      # coercion =
+      #   ActiveadminSettingsCached::Coercions.new(options[:template_object].defaults)
 
       content title: options[:title] do
         render partial: options[:template], locals: { settings_model: options[:template_object] }
@@ -29,9 +27,13 @@ module ActiveadminSettingsCached
       page_action :update, method: :post do
         settings_params = params.require(:settings).permit!
 
-        coercion.cast_params(settings_params) do |name, value|
-          options[:template_object].save(name, value)
+        settings_params.each do |field_name, value|
+          options[:template_object].save(field_name, value)
         end
+
+        # coercion.cast_params(settings_params) do |name, value|
+        #   options[:template_object].save(name, value)
+        # end
 
         flash[:success] = t('activeadmin_settings_cached.settings.update.success'.freeze)
         Rails.version.to_i >= 5 ? redirect_back(fallback_location: admin_root_path) : redirect_to(:back)
